c6ba1f8458df583dd523d1298083d17b2d270987
Justin Reyes <justin.regi.reyes@gmail.com>
2015-09-18 17:30:28 -0700

    A controller method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get '/creatures' => 'creatures#index'
   get '/creatures/new' => 'creatures#new'
   post '/creatures' => 'creatures#create'
+
+  #rake routes to check these routes
+  get '/creatures/:id' => 'creatures#show'
+  get '/creatures' => 'creatures#create'
 end
 
 
