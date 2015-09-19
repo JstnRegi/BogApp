4328c15d04c6ba6dc9520dd5784a11f334d6119b
Justin Reyes <justin.regi.reyes@gmail.com>
2015-09-18 17:09:39 -0700

    A create Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'creatures#index'
   get '/creatures' => 'creatures#index'
   get '/creatures/new' => 'creatures#new'
+  post '/creatures' => 'creatures#create'
 end
+
+
